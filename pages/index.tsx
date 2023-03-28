d3325578d71a761946c6ba222ff9b001cc462287
ADRIAN HERNANDEZ <adrianhh1426@gmail.com>
2023-03-27 22:19:17 -0600

    probando function getStaticProps

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,24 @@
 import { Layout } from "@/layouts";
 import { Button } from "@nextui-org/react";
+import { GetStaticProps } from "next";
 
-export default function Home() {
+export default function Home(props) {
+
+  console.log('props: ', props);
+
   return (
     <Layout title="Listado de pokemones">
       <Button color='gradient'>mi botón</Button>
     </Layout>
   )
 }
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  console.log('en funcion staticpropps')
+
+  return {
+    props: {
+
+    }
+  }
+};
