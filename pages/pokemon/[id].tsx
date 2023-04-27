24297f6a6128f0897038eb5572167e36ff7f6240
adrianhh1426 <adrianhh1426@gmail.com>
2023-04-27 16:18:46 -0600

    generando las 151 pagina

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,43 +1,40 @@
+import pokemonApi from "@/apis/pokemonApi";
+import { PokemonDetail } from "@/interfaces/iPokemonDetail";
 import { Layout } from "@/layouts";
 import { GetStaticPaths, GetStaticProps } from "next";
-import { useRouter } from "next/router";
 
 interface Props {
-  id: string;
-  name: string;
+  pokemon: PokemonDetail
 };
 
 const PokemonDetailPage = (props: Props) => {
-  const { id, name } = props;
-  const router = useRouter();
-
-  console.log(router.query)
+  const { pokemon } = props;
 
   return (
     <Layout title="Algún pokemon" >
-      <h1>{id} - {name}</h1>
+      <h1>{pokemon.name}</h1>
     </Layout>
   );
 };
 
 export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  const pokemones = [...Array(151)].map((value, index) => `${index + 1}`);
+
   return {
-    paths: [{
-      params: {
-        id: '1'
-      }
-    }],
+    paths: pokemones.map(id => ({
+      params: { id }
+    })),
     fallback: false
   };
 };
 
 export const getStaticProps: GetStaticProps = async (ctx) => {
-  // const resApi = await pokemonApi.get<PokemonResponse>('/pokemon?limit=151');
+  const { params } = ctx;
+  const resApi = await pokemonApi.get<PokemonDetail>(`/pokemon/${params!.id}`);
 
   return {
     props: {
-      id: 1,
-      name: 'Bulbazor'
+      pokemon: resApi.data
     }
   }
 };
