8e7d74d4254fa0f99e1ff7b5d1fa12685477a0ca
adrianhh1426 <adrianhh1426@gmail.com>
2023-04-27 14:55:20 -0600

    getStaticPaths

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,15 +1,45 @@
+import { Layout } from "@/layouts";
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router";
 
-const PokemonDetail = () => {
+interface Props {
+  id: string;
+  name: string;
+};
+
+const PokemonDetailPage = (props: Props) => {
+  const { id, name } = props;
   const router = useRouter();
 
   console.log(router.query)
 
   return (
-    <div>
-      Pokemon detail
-    </div>
+    <Layout title="Algún pokemon" >
+      <h1>{id} - {name}</h1>
+    </Layout>
   );
 };
 
-export default PokemonDetail;
+export const getStaticPaths: GetStaticPaths = async (ctx) => {
+  return {
+    paths: [{
+      params: {
+        id: '1'
+      }
+    }],
+    fallback: false
+  };
+};
+
+export const getStaticProps: GetStaticProps = async (ctx) => {
+  // const resApi = await pokemonApi.get<PokemonResponse>('/pokemon?limit=151');
+
+  return {
+    props: {
+      id: 1,
+      name: 'Bulbazor'
+    }
+  }
+};
+
+export default PokemonDetailPage;
