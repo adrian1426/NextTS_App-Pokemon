1748e3c7cb96e8167611d8813cbd606d5a5ecf59
adrianhh1426 <adrianhh1426@gmail.com>
2023-04-28 13:34:48 -0600

    agregando rutas de favoritos y home

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,5 +1,6 @@
-import { Spacer, Text, useTheme } from "@nextui-org/react";
+import { Link, Spacer, Text, useTheme } from "@nextui-org/react";
 import Image from "next/image";
+import NextLink from 'next/link';
 
 const Navbar = () => {
   const { theme } = useTheme();
@@ -14,18 +15,26 @@ const Navbar = () => {
       padding: '0 20px',
       backgroundColor: theme?.colors.gray900.value
     }}>
-      <Image
-        alt="imagen"
-        src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png'
-        height={70}
-        width={70}
-      />
 
-      <Text color="black" h2>P</Text>
-      <Text color="black" h3>ókemon</Text>
+
+      <NextLink href='/' passHref>
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          <Image
+            alt="imagen"
+            src='https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/back/132.png'
+            height={70}
+            width={70}
+          />
+          <Text color="black" h2>P</Text>
+          <Text color="black" h3>ókemon</Text>
+        </div>
+      </NextLink>
 
       <Spacer css={{ flex: 1 }} />
-      <Text color="black">Favoritos</Text>
+
+      <NextLink href='/favoritos' passHref>
+        <Text color="black">Favoritos</Text>
+      </NextLink>
     </div>
   );
 };
---
+++ pages/favoritos/index.tsx
@@ -0,0 +1,12 @@
+import { Layout } from '@/layouts';
+import React from 'react';
+
+const index = () => {
+  return (
+    <Layout title='Favoritos'>
+      <h1>Favoritos</h1>
+    </Layout>
+  );
+};
+
+export default index;
